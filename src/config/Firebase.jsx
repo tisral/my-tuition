4865de93f6fce3823dd1886169f9ce9fb1a3f4f9
tisral <64937810+tisral@users.noreply.github.com>
2025-02-19 13:32:21 -0600

    config updated

--- src/config/Firebase.jsx
+++ src/config/Firebase.jsx
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBP1GK4o3qvjatgtiZ_mRec44mTYWUgrco",
-  authDomain: "mytuition-82223.firebaseapp.com",
-  projectId: "mytuition-82223",
-  storageBucket: "mytuition-82223.appspot.com",
-  messagingSenderId: "941743831182",
-  appId: "1:941743831182:web:5797d67eeda9f4cb96b1df",
-  measurementId: "G-LNG9W637SC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
